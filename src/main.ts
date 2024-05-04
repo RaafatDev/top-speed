0c1a816a7446213181fe7f96ed06dea8d96b158a
Raafat-itu <raafat@innovation-through-understanding.de>
2024-05-04 15:51:47 +0200

    initial commit - created vite project and added sass package

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+// import "./style.scss";
+// @import "./style.scss"
+// import typescriptLogo from "./typescript.svg";
+// import viteLogo from "/vite.svg";
+// import { setupCounter } from "./counter.ts";
+
+console.log("hello world");
+
+// document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+
+// `;
+
+// setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
